0c7a729e61818481468a0ccd71699797bed4e9ae
MichaBe <micha.beierl@googlemail.com>
2012-05-24 22:20:56 +0200

    Advanced DB-Connector Hinzufügen der Standardfunktionen und Eigenschaften, Konstruktor, Destruktor, globalConfig u. connectionID

--- cms/backend/a_dbconnector.php
+++ cms/backend/a_dbconnector.php
@@ -5,6 +5,24 @@
 	
 	class advanced_dbconnector {
 		private $connectionID;
+		private $globalConfig;
+		
+		public function __construct() {
+			$this->globalConfig = $GLOBALS["globalConfig"];				
+			$this->connection_ID = mysql_connect($this->globalConfig["db_server"], $this->globalConfig["db_user"], $this->globalConfig["db_passwd"]);
+			
+			if($this->connection_ID != FALSE) {
+				mysql_query(sprintf("USE %s", mysql_real_escape_string($this->globalConfig["db_scheme"])), $this->connection_ID);
+			}
+			else {
+				return FALSE;
+			}
+		}
+		
+		public function __desctruct() {
+			mysql_close($this->connection_ID);
+		}
+		
 		
 	}
 ?>
