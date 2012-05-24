ca7fa5c5a082145d6aa33f60a72c97066cbac341
MichaBe <micha.beierl@googlemail.com>
2012-05-24 17:53:55 +0200

    Errorhandling Einrichten des Errorhandlings, erstellen der error404.php, testen des Aufrufes aus index.php und snippet.php, funktioniert zu diesem Zeitpunkt Problemlos

---
+++ error404.php
@@ -0,0 +1,61 @@
+<?php
+	include("cms/backend/dbconnector.php");
+	$myConnector = new simple_dbconnector();
+	$Konfiguration = $myConnector->getKonfiguration();
+	$allKategories = $myConnector->getAllKategorien();
+	$currentSnippet = $myConnector->getOneBeitrag(404);
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<?php
+			echo '<title>'.$Konfiguration[0]['PageTitle'].' - '.$currentSnippet[0]['Sheadline'].'</title>';
+			echo '<meta name="keywords" content="'.$currentSnippet[0]['Skeywords'].'" />';
+			echo '<meta name="description" content="'.$currentSnippet[0]['Sshorttext'].'" />';
+			echo '<link rel="stylesheet" type="text/css" href="/cms/style/'.$Konfiguration[0]['StyleName'].'/theme.css" />';
+		?>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="header">
+				<?php
+					echo $Konfiguration[0]['PageTitle'];
+				?>
+			</div>
+			<div class="categories">
+				<ul>
+					<?php
+						foreach ($allKategories as $curKat) {
+							echo '<li id="C'.$curKat['CID'].'"><a href="index.php?CID='.$curKat['CID'].'">'.$curKat['Cname'].'</a></li>';
+						}
+					?>
+				</ul>
+			</div>
+			<div class="inhalt">
+				<?php
+					echo '<div class="snippet" id="'.$currentSnippet[0]['SID'].'">';
+					echo '<h2>'.$currentSnippet[0]['Sheadline'].'</h2>';
+					echo '<div class="s_header">';
+					echo '</div>';
+					echo $currentSnippet[0]['Stext'];
+					echo '</div>';
+				?>
+			</div>
+			<div class="footer">
+				<ul>
+					<?php
+						$footer = $myConnector->getforFooter();
+						foreach ($footer as $shorts) {
+							echo '<li><a href="snippet.php?SID='.$shorts['SID'].'">'.$shorts['Sheadline'].'</a></li>';
+						}
+					?>
+				</ul>
+			</div>
+		</div>
+	</body>
+	<?php
+		unset($myConnector);
+	?>
+</html>
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include "/cms/backend/dbconnector.php";
-	$mDBc = new simple_dbconnector();
-	$result = $mDBc->getOneBeitrag(3);
-	var_dump($result);
-	unset($mDBc);
-?>
