161fbd79aa3fb226fcca7f7b675a81a71f356dec
MichaBe <micha.beierl@googlemail.com>
2012-05-31 22:04:13 +0200

    Änderungen an readme, error-Konzept und Style
    Hinzufügen einer Installationsanleitung
    Error404 hat ab sofort SID 1 statt 404 (Sonst vergibt AutoIncrement nur
    IDs höher 404)

--- cms/management/beitraege/index.php
+++ cms/management/beitraege/index.php
@@ -21,7 +21,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>myCMSxml</title>
-		<link rel="stylesheet" type="text/css" href="/cms/management/cms.css" />
+		<link rel="stylesheet" type="text/css" href="../cms.css" />
 	</head>
 	<body>
 		<div class="wrapper">
