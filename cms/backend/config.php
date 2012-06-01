251b768a6972e11079994cd50a622f836f3d1cb9
MichaBe <micha.beierl@googlemail.com>
2012-06-01 22:42:59 +0200

    Commit for shell

--- cms/backend/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-		return array(
-			"db_user" => "root",			/* Bitte hier den Benutzernamen angeben */
-			"db_passwd" => "",				/* Bitte hier ihr Passwort angeben */
-			"db_scheme" => "mycmsxml",		/* Bitte hier den Namen der benutzten Datenbank angeben */
-			"db_server" => "localhost",		/* Bitte hier den Datenbankserver angeben */
-		);
-?>
--- cms/management/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$_SESSION = array();
-	session_destroy();
-	header('Location: http://'.$_SERVER['HTTP_HOST'].'/snippet.php?SID=1');
-?>
