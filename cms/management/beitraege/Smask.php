9cf113730e917dba20893f2b6a4d51d31911ebbb
MichaBe <micha.beierl@googlemail.com>
2012-06-06 13:03:45 +0200

    Styleanpassungen
    Anpassungen am Management-Style
    Strukturelle Anpassungen der Smask
    Aktivieren und auswählen des noteMe-Styles
    Anpassungen des Stylesheets an die Struktur
    Test des Stylesheets

--- cms/management/beitraege/Smask.php
+++ cms/management/beitraege/Smask.php
@@ -41,37 +41,48 @@
 	</head>
 	<body>
 		<div class="wrapper">
-			<?php
-				echo '<h1>Herzlich willkommen, '.$currentUser[0]['Uname'].'</h1>';
-			?>
-			<h2>Beitrag bearbeiten</h2>
-			<a href="index.php">zur&#252;ck zur Beitrags&#252;bersicht</a>
-			<form action="save.php" method="post">
-				<?php
-					echo '&#220;berschrift: <input name="headline" type="text" size="30" maxlength="30" value="'.$currentBeitrag[0]['Sheadline'].'" />';
-					
-					$allKategorien = $myADBConnector->getChoosableKategorien();
-					echo '<select name="cid" size="1">';
-					foreach($allKategorien as $Kategorie) {
-						if ($Kategorie['CID'] == $currentBeitrag[0]['CID'])
-							echo '<option value="'.$Kategorie['CID'].'" selected>'.$Kategorie['Cname'].'</option>';
-						else
-							echo '<option value="'.$Kategorie['CID'].'">'.$Kategorie['Cname'].'</option>';
-					}
-					echo '</select><br />';
-					
-					echo 'Schl&#252;sselw&#246;rter: <input name="keywords" type="text" size="60" maxlength="60" value="'.$currentBeitrag[0]['Skeywords'].'" /><br />';
-					echo 'Vorschautext:<br />';
-					echo '<textarea name="shorttext">'.$currentBeitrag[0]['Sshorttext'].'</textarea><br />';
-					echo 'Ihr Beitrag:<br />';
-					echo '<textarea name="text">'.$currentBeitrag[0]['Stext'].'</textarea>';
-					if(!$newBeitrag)
-						echo '<input type="hidden" name="ID" value="'.$_GET['SID'].'" />';
-				?>
-				<br />
-				<input type="submit" value="speichern" />
-				<input type="reset"  value="zur&#252;cksetzen" />
-			</form>
+			<div class="navigation">
+				<ul>
+					<?php
+						echo '<li><a href="../">Zur Startseite</a></li>';
+						for($i = 1; $i < count($currentRights); $i++) {
+							if($currentRights[$i]['Xvalue'] == 1)
+								echo '<li><a href="../'.$currentRights[$i]['Rshort'].'/">'.$currentRights[$i]['Rtopic'].'</a></li>';
+						}						
+						echo '<li><a href="../logout.php">Vom System abmelden</a></li>';
+					?>
+				</ul>
+			</div>
+			<div class="inhalt">
+				<h2>Bearbeiten oder erstellen Sie hier die Beitr&#228;ge</h2>
+				<form action="save.php" method="post">
+					<?php
+						echo '&#220;berschrift: <input name="headline" type="text" size="30" maxlength="30" value="'.$currentBeitrag[0]['Sheadline'].'" />';
+						
+						$allKategorien = $myADBConnector->getChoosableKategorien();
+						echo '<select name="cid" size="1">';
+						foreach($allKategorien as $Kategorie) {
+							if ($Kategorie['CID'] == $currentBeitrag[0]['CID'])
+								echo '<option value="'.$Kategorie['CID'].'" selected>'.$Kategorie['Cname'].'</option>';
+							else
+								echo '<option value="'.$Kategorie['CID'].'">'.$Kategorie['Cname'].'</option>';
+						}
+						echo '</select><br />';
+						
+						echo 'Schl&#252;sselw&#246;rter: <input name="keywords" type="text" size="60" maxlength="60" value="'.$currentBeitrag[0]['Skeywords'].'" /><br />';
+						echo 'Vorschautext:<br />';
+						echo '<textarea name="shorttext" id="shorttext">'.$currentBeitrag[0]['Sshorttext'].'</textarea><br />';
+						echo 'Ihr Beitrag:<br />';
+						echo '<textarea name="text" id="text">'.$currentBeitrag[0]['Stext'].'</textarea>';
+						if(!$newBeitrag)
+							echo '<input type="hidden" name="ID" value="'.$_GET['SID'].'" />';
+					?>
+					<br />
+					<input type="submit" value="speichern" />
+					<input type="reset"  value="zur&#252;cksetzen" />
+				</form>	
+			</div>
+			<?php include('../../backend/formanagement/getfooter.php'); ?>
 		</div>
 	</body>
 	<?php
