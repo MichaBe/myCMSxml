8b283642b003a34bd28663be3e067dab5d5cdd3f
MichaBe <micha.beierl@googlemail.com>
2012-06-29 22:15:26 +0200

    Versionierung, aktuelle Version: 0.0.3
    Änderung im Init-Script und in der Readme-Datei

--- cms/backend/init.sql
+++ cms/backend/init.sql
@@ -117,7 +117,7 @@ INSERT INTO Berechtigung VALUES(NULL, 2, 5, FALSE);
 INSERT INTO Berechtigung VALUES(NULL, 2, 6, FALSE);
 
 
-INSERT INTO Konfiguration VALUES(1, 'konservativ', 'myCMS.xml', '0.0.1', 'deutsch', 'Hier wurde noch kein Beitrag ver&#246;ffentlicht');
+INSERT INTO Konfiguration VALUES(1, 'konservativ', 'myCMS.xml', '0.0.3', 'deutsch', 'Hier wurde noch kein Beitrag ver&#246;ffentlicht');
 
 
 INSERT INTO Beitrag VALUES(1, 1, 1, 'myCMSxml', NULL, '<p align="center"><form action="cms/management/login.php" method="post"><table><tr><td>Benutzername:</td><td><input type="text" name="username"/></td></tr><tr><td>Passwort:</td><td><input type="password" name="passwort"/></td></tr></table><input type="submit" value="Anmelden"/></form></p>', '2012-05-20', '2012-05-20', NULL);
